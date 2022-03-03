b39169a0a8a3c04b9e84ec3abe64b0d3332d3f62
Terror <89325375+TerrorGaming473@users.noreply.github.com>
2022-03-03 20:20:09 -0300

    Update CreditsState.hx

--- source/CreditsState.hx
+++ source/CreditsState.hx
@@ -38,7 +38,7 @@ class CreditsState extends MusicBeatState
 	{
 		#if desktop
 		// Updating Discord Rich Presence
-		DiscordClient.changePresence("In the Menus", null);
+		DiscordClient.changePresence("In Credit Menu", null);
 		#end
 
 		bg = new FlxSprite().loadGraphic(Paths.image('menuDesat'));
